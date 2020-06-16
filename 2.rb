cd90639904031f2825caf01f4394ebd580782be2
HAN O <a.ocal88@gmail.com>
2020-06-16 01:06:09 +0000

    Automatically backed up by Learn

--- 2.rb
+++ 2.rb
@@ -10,3 +10,5 @@ while num_of_hotdog_eaten < 7
   num_of_hotdog_eaten += 1
   puts "You have now eaten #{num_of_hotdog_eaten} hot dog(s)."
 end
+
+puts "You ate a total of #{num_of_hotdog_eaten} hot dog(s)"
